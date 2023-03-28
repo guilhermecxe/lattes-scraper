ad6285fbc8ac4516d351fededeb6ce5700f85b34
Guilherme Alves <guilhermecxe@hotmail.com>
2023-03-27 21:57:23 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Guilherme Alves",
+    description="A package to scrape data from Plataforma Lattes.",
+    name="lattes_parser",
+    version="0.1.0",
+    packages=find_packages(include=["lattes_scraper"]),
+    install_requires=[
+        'selenium==4.8.3',
+    ],
+    python_requires='>=3.10',
+)
